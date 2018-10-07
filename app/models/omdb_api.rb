57dddc35b9571a24687ab2d33210da76851e5f91
Thibaud RENAUX <thibaud.renaux@gmail.com>
2018-10-07 18:23:02 +0200

    Use Poster data for item picture

--- app/models/omdb_api.rb
+++ app/models/omdb_api.rb
@@ -12,12 +12,27 @@ end
 module OmdbApi
   class Search
     require 'nokogiri'
+    require 'open-uri'
     API_KEY = '?apikey=f032e0a4'
 
     def self.item_by_title(title)
       result = JSON.parse OmdbApi.omdb_client[API_KEY].get({params:{t: title}})
-      if result.present? && result.is_a?(JSON)
-        OmdbItem.create(result)
+      if result.present? && result.is_a?(Hash) && result.keys.any?
+        valid_keys = OmdbItem.column_names
+        result2 = {}
+        result.each_pair do |k,v|
+          if valid_keys.include?(k.downcase)
+            result2.merge!({k.downcase => v})
+          end
+        end
+        item = OmdbItem.create(result2) if result2.keys.any?
+        if result.key?('Poster')
+          file = open(result['Poster'])
+          if file.present?
+            item.poster.attach(io: file, filename: "#{item.title}.jpg")
+          end
+        end
+        return item
       end
       return result
     end
