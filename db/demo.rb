875d1a0ee4f8f05db0e46d877d51a7704b985a0b
Thibaud RENAUX <thibaud.renaux@gmail.com>
2018-11-04 17:42:44 +0100

    bundle movies for db demo

--- db/demo.rb
+++ db/demo.rb
@@ -2,3 +2,7 @@ Faker::Config.locale = :en
 12.times do
   OmdbApi::Search.item_by_title Faker::Book.title
 end
+
+File.open("db/movies.txt", "r").each_line do |movie_title|
+  OmdbApi::Search.item_by_title movie_title
+end
